3ac5df40ad508647b16ed9a243e9f53b42735a70
陈晓宁 <1120751813@qq.com>
2015-05-05 10:47:00 +0800

    修改二维码逻辑

---
+++ JuranClient/JuranClient/Controllers/QRCode/QRCodeWebViewController.h
@@ -0,0 +1,15 @@
+//
+//  QRCodeWebViewController.h
+//  JuranClient
+//
+//  Created by 陈晓宁 on 15/5/5.
+//  Copyright (c) 2015年 Juran. All rights reserved.
+//
+
+#import "ALViewController.h"
+
+@interface QRCodeWebViewController : ALViewController
+
+@property (strong, nonatomic) NSString * requestURL;
+
+@end
