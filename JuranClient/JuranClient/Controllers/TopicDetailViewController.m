f883d3c2de46ce9ba14d72d7ef1b392bd15a078f
HuangKai <HuangKai@Jayce-YangdeiMac.local>
2015-05-11 10:47:21 +0800

    宏修改

--- JuranClient/JuranClient/Controllers/TopicDetailViewController.m
+++ JuranClient/JuranClient/Controllers/TopicDetailViewController.m
@@ -16,7 +16,7 @@
 #import "CanRemoveImageView.h"
 #import "ShareView.h"
 #import "IQKeyboardManager.h"
-
+#import "UIViewController+Menu.h"
 
 @interface TopicDetailViewController ()<UITableViewDataSource, UITableViewDelegate, CommentCellDelegate, UITextFieldDelegate, ALWebViewDelegate, CanRemoveImageViewDelegate>
 {
@@ -28,14 +28,11 @@
 @property (nonatomic, strong) IBOutlet UIView *headerView;
 @property (nonatomic, strong) IBOutlet UILabel *titleLabel;
 @property (nonatomic, strong) IBOutlet UILabel *timeLabel;
-@property (nonatomic, strong) IBOutlet UILabel *viewCountLabel;
-@property (nonatomic, strong) IBOutlet UILabel *commentCountLabel;
 @property (nonatomic, strong) IBOutlet UIButton *viewCountButton;
 @property (nonatomic, strong) IBOutlet UIButton *commentCountButton;
 
 @property (nonatomic, strong) IBOutlet UIView *commentView;
 @property (nonatomic, strong) IBOutlet UITextField *commentTextField;
-@property (nonatomic, strong) IBOutlet UIView *bottomView;
 @property (nonatomic, strong) IBOutlet ALWebView *contentWebView;
 @property (nonatomic, strong) UIButton *hiddenButton;
 
@@ -65,7 +62,7 @@
     [[NSBundle mainBundle] loadNibNamed:NSStringFromClass([self class]) owner:self options:nil];
     self.openStatusDic = [NSMutableDictionary dictionary];
     self.navigationItem.title = @"话题详情";
-    [self configureRightBarButtonItemImage:[UIImage imageNamed:@"menu_icon_fenxiang.png"] rightBarButtonItemAction:@selector(onShare:)];
+    [self configureRightBarButtonItemImage:[UIImage imageNamed:@"icon-dot"] rightBarButtonItemAction:@selector(onShare:)];
     
     [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(keyboardWillShow:)name:UIKeyboardWillShowNotification object:nil];
     [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(keyboardWillBeHidden:)name:UIKeyboardWillHideNotification object:nil];
@@ -378,11 +375,14 @@
     [self.navigationController pushViewController:vc animated:YES];
 }
 
-- (IBAction)onShare:(id)sender{
-    [[ShareView sharedView] showWithContent:_topic.theme image:nil title:_topic.theme url:[NSString stringWithFormat:@"http://apph5.juran.cn/topics/%@%@", _topic.topicId, [Public shareEnv]]];
+- (void)onShare:(id)sender{
+    [self showAppMenu:^{
+        [[ShareView sharedView] showWithContent:_topic.theme image:nil title:_topic.theme url:[NSString stringWithFormat:@"http://apph5.juran.cn/topics/%@%@", _topic.topicId, [Public shareEnv]]];
+    }];
+    
 }
 
-- (IBAction)onComment:(id)sender{
+- (void)onComment:(id)sender{
     self.selectComment = nil;
     _commentView.hidden = NO;
     [_commentTextField becomeFirstResponder];
