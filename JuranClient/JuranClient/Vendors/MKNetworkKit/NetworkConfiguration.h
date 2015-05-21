8128d0eb9d9f55e69a061ba63fefe8496c8b0d25
pengchuan <pengchuan@sephplus.com>
2015-05-21 15:20:22 +0800

    no message

--- JuranClient/JuranClient/Vendors/MKNetworkKit/NetworkConfiguration.h
+++ JuranClient/JuranClient/Vendors/MKNetworkKit/NetworkConfiguration.h
@@ -20,12 +20,12 @@
 
 
 /* UAT */
-//#define JR_IMAGE_SERVICE  @"http://image1.juran.cn"
-//#define JR_SERVICE  @"http://54.223.161.28:8080"
+#define JR_IMAGE_SERVICE  @"http://image1.juran.cn"
+#define JR_SERVICE  @"http://54.223.161.28:8080"
 
 //SIT内网
-#define JR_SERVICE  @"http://10.199.5.241:8083"
-#define JR_IMAGE_SERVICE  @"http://10.199.5.57:8080"
+//#define JR_SERVICE  @"http://10.199.5.241:8083"
+//#define JR_IMAGE_SERVICE  @"http://10.199.5.57:8080"
 //SIT外网
 //#define JR_SERVICE  @"http://124.193.207.194:8093"
 //#define JR_IMAGE_SERVICE  @"http://124.193.207.194:8081"
